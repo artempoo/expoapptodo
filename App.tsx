75715803bc09323351436d04160add5d88a4f848
artempoo <77047966+artempoo@users.noreply.github.com>
2026-05-31 01:48:56 +0300

    init expo app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const handlerButton = () => {
+    console.log("кнопка работает");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>TODO app | мое первое приложение</Text>
+      <Image src="./assets/icon.png" style={{ width: 200, height: 200 }} />
+      <TouchableOpacity style={styles.button} onPress={handlerButton}>
+        <Text style={styles.buttonText}>Нажми на меня</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +26,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#ccc",
+  },
+  buttonText: {
+    color: "#f00",
   },
 });
